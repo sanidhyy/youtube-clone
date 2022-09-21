b807159f25e6c77bfaefe906ba27b08f562c331a
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-09-21 17:40:00 +0530

    video details page done

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,9 +3,10 @@ import { Stack, Box } from "@mui/material";
 
 import { VideoCard, ChannelCard } from "./";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction = "row" }) => {
+  if (!videos?.length) return "Loading...";
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack direction={direction} flexWrap="wrap" justifyContent="start" gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id?.videoId && <VideoCard video={item} />}
