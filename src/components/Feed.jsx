d4853e0fec5454c5d9aeb63604541e0213e5d14f
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-09-21 14:35:02 +0530

    videos component created

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
   return (
@@ -20,6 +20,19 @@ const Feed = () => {
         >
           Copyright 2022 YouTube Clone
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "#fff" }}
+        >
+          New <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
