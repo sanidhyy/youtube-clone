d0cba03bdd7b854f4d3b94d6b82fa0e03f3d58c2
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-09-17 15:02:00 +0530

    chore(deps): upgrade react-player from v2 to v3
    Follow the official v3 migration by renaming the player url prop to src.

--- src/components/VideoDetail.tsx
+++ src/components/VideoDetail.tsx
@@ -41,7 +41,7 @@ const VideoDetail = () => {
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
             <ReactPlayer
-              url={`https://www.youtube.com/watch?v=${id}`}
+              src={`https://www.youtube.com/watch?v=${id}`}
               className="react-player"
               controls
             />
